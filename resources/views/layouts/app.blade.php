111e6fbcac98b10d8bf03285209a4f950e87b6eb
Sijan Bhattarai <sijan@accessworld.net>
2018-02-05 19:46:45 +0545

    Unsafe scripts removed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,8 +21,8 @@
     <link href="{{asset('css/main-style.css')}}" rel="stylesheet">
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <script src="{{asset('js/html5shiv.min.js')}}"></script>
+    <script src="{{asset('js/respond.min.js')}}"></script>
     <![endif]-->
     <!-- Icon Font-->
     <link href="{{asset('css/style.css')}}" rel="stylesheet">
